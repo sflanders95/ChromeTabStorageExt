ae035c2f429a7e06ce3fd6ecd9a45b71ab067f7b
Steven Flanders <Steve.flanders@live.com>
2018-09-03 21:46:04 -0400

    init

---
+++ js/bakground.js
@@ -0,0 +1,10 @@
+'use strict';
+
+console.log('background.js has started.');
+
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.sync.set({color: '#3aa757'}, function() {
+      console.log("The color is green.");
+    });
+  });
+
